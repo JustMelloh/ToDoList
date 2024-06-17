4d323a773c673c33a3db11f669c14923371c593f
Austin <Austin.reid@keyin.com>
2024-06-17 18:57:24 -0230

    Allow for Login and Registration for User to be able to Add, Complete tasks.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,26 +19,54 @@ public class Main {
             System.out.println("1. Add Task.");
             System.out.println("2. Mark Task as Complete.");
             System.out.println("3. Print All Tasks.");
-            System.out.println("4. Exit.");
+            System.out.println("4. Create User. ");
+            System.out.println("5. Login. ");
+            System.out.println("6. Exit.");
             System.out.println("Enter desired choice: ");
             int choice = scanner.nextInt();
             scanner.nextLine();
 
             switch (choice) {
                 case 1:
-                    System.out.println("Enter the task description: ");
-                    String description = scanner.nextLine();
-                    taskList.addTask(description);
+                    if (user.isLoggedIn()) {
+                        System.out.println("Enter the task description: ");
+                        String description = scanner.nextLine();
+                        taskList.addTask(description);
+                    } else {
+                        System.out.println("You must be logged in to add a task.");
+                    }
                     break;
                 case 2:
-                    System.out.println("Enter the index of the task you want to mark as complete: ");
-                    int index = scanner.nextInt();
-                    taskList.markTaskAsComplete(index);
+                    if (user.isLoggedIn()) {
+                        System.out.println("Enter the index of the task you want to mark as complete: ");
+                        int index = scanner.nextInt();
+                        taskList.markTaskAsComplete(index);
+                    } else {
+                        System.out.println("You must be logged in to mark a task as complete.");
+                    }
                     break;
                 case 3:
                     taskList.printAllTasks();
                     break;
                 case 4:
+                    System.out.println("Enter the username: ");
+                    String username = scanner.nextLine();
+                    System.out.println("Enter the password: ");
+                    String password = scanner.nextLine();
+                    user = new UserClass(username, password);
+                    break;
+                case 5:
+                    System.out.println("Enter the username: ");
+                    String loginUsername = scanner.nextLine();
+                    System.out.println("Enter the password: ");
+                    String loginPassword = scanner.nextLine();
+                    if (user.login(loginUsername, loginPassword)) {
+                        System.out.println("Login successful.");
+                    } else {
+                        System.out.println("Login failed.");
+                    }
+                    break;
+                case 6:
                     running = false;
                     break;
 
--- src/main/java/UserClass.java
+++ src/main/java/UserClass.java
@@ -5,12 +5,14 @@ public class UserClass {
     /* Instantiate the Username and Password as a String*/
     private String username;
     private String password;
+    private boolean loggedIn;
 
     /* Constructor for UserClass */
 
     public UserClass(String username, String password) {
         this.username = username;
         this.password = password;
+        this.loggedIn = false;
     }
 
     /* Method to return the username of the user */
@@ -29,5 +31,19 @@ public class UserClass {
 
     public String toString() {
         return this.username + " - " + this.password;
+    }
+
+    /* Allows for correct Login*/
+    public boolean login(String loginUsername, String loginPassword) {
+        if (this.username.equals(loginUsername) && this.password.equals(loginPassword)) {
+            this.loggedIn = true;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isLoggedIn(){
+        return this.loggedIn;
     }
 }
